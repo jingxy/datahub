7cbd9391d490f4e4134de528c8edfcc05660135b
zonesan <san@zoobook.org>
2015-12-01 11:53:14 +0800

    add file monit mechanism

--- daemon/monitor.go
+++ daemon/monitor.go
@@ -1,13 +1,65 @@
 package daemon
 
-var monitList map[string]string
+import (
+	"fmt"
+	log "github.com/asiainfoLDP/datahub/utils/clog"
+	"github.com/asiainfoLDP/datahub/utils/fsnotify"
+	"github.com/asiainfoLDP/datahub/utils/logq"
+)
+
+var monitList = []string{
+	"/var/lib/datahub",
+	"/tmp/foo",
+	"/tmp/bar",
+}
 
 func datapoolMonitor() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		l := log.Error(err)
+		logq.LogPutqueue(l)
+	}
+	defer watcher.Close()
 
+	initMonitList()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					l := log.Warn("modified file:", event.Name)
+					logq.LogPutqueue(l)
+				}
+				if event.Op&fsnotify.Remove == fsnotify.Remove {
+					l := log.Warn("deleted file:", event.Name)
+					logq.LogPutqueue(l)
+				}
+				if event.Op&fsnotify.Rename == fsnotify.Rename {
+					l := log.Warn("renamed file:", event.Name)
+					logq.LogPutqueue(l)
+				}
+			case err := <-watcher.Errors:
+				l := log.Error("error:", err)
+				logq.LogPutqueue(l)
+			}
+		}
+	}()
+
+	for _, filecheck := range monitList {
+		err = watcher.Add(filecheck)
+		if err != nil {
+			l := log.Error(err)
+			logq.LogPutqueue(l)
+		}
+	}
+
+	<-done
 }
 
-/*
-func init() {
-	fmt.Println("TODO init monitor list from local db.")
+func initMonitList() {
+	fmt.Println("TODO INIT MONIT LIST.")
 }
-*/
